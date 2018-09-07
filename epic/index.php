a4bfcf4435f7328d334be68a6890fddc6a62090d
Kevin Atkins <kevindewayneatkins@gmail.com>
2018-09-06 22:35:54 -0700

    initial commit

---
+++ epic/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="description" content="main page for epic"/>
+		<link href="styles/persona.css" rel="stylesheet" type="text/css"/>
+		<title>Epic Page</title>
+	</head>
+	<body>
+
+	</body>
+	<footer>
+
+	</footer>
+</html>
