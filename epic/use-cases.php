c2eb21f01cebea946ad05de07b69219ea124d288
Kevin Atkins <kevindewayneatkins@gmail.com>
2018-09-07 16:57:17 -0700

    finished Dante's interaction flow and updated use-cases.php

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -23,18 +23,25 @@
 			<h2>Serena's Interaction Flow</h2>
 			<div class="flowlist">
 				<ol>
-					<li>Serena accesses site</li>
-					<li>She logs into account</li>
+					<li>Serena accesses site.</li>
+					<li>She logs into account.</li>
 					<li>Site directs her to her profile dashboard which presents outreach forms assigned to her profile.</li>
 					<li>She selects one of the assigned forms that presents an option to view contacts previously obtained or to initiate outreach mode.</li>
 					<li>If she selects the option to view contacts obtained, application will then navigate and display contacts with the abilty to filter by date, city, state, & gender.</li>
-					<li>If she selects the option to initiate "outreach" mode, the application will open a form for individuals to input requested personal information</li>
+					<li>If she selects the option to initiate "outreach" mode, the application will open a form for individuals to input requested personal information.</li>
+					<li>Serena encounters Dante and ask if he has a moment and interested in being contacted regarding local events.</li>
 				</ol>
 			</div>
 			<h2>Dante's Interaction Flow</h2>
 			<div class="flowlist">
 				<ol>
-					<li></li>
+					<li>Dante is handed a mobile device from Serena for him to input his contact information.</li>
+					<li>Dante views the form displayed on the mobile device utilized for "outreach" mode.</li>
+					<li>Dante fills out input fields of the form that are relative to his personal contact information.</li>
+					<li>Each input field he submits transitions to another input field upon completion.</li>
+					<li>He can go back to a prior input fields if personal information was entered in error.</li>
+					<li>He can decline submitting the form anytime if he changes his mind.</li>
+					<li>If he completes the form without encountering the last two scenarios, he can submit the information entered.</li>
 				</ol>
 			</div>
 		</main>
