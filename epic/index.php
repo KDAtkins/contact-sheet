ebaa30047a4b5575727b37a1b95e2a8b34d23e27
Kevin Atkins <kevindewayneatkins@gmail.com>
2018-09-06 22:41:01 -0700

    minor changes to index.php

--- epic/index.php
+++ epic/index.php
@@ -3,11 +3,18 @@
 	<head>
 		<meta charset="utf-8"/>
 		<meta name="description" content="main page for epic"/>
-		<link href="styles/persona.css" rel="stylesheet" type="text/css"/>
+		<link href="" rel="stylesheet" type="text/css"/>
 		<title>Epic Page</title>
 	</head>
 	<body>
+		<header>
+			<nav class="topnav">
 
+			</nav>
+		</header>
+		<main>
+
+		</main>
 	</body>
 	<footer>
 
