2e4683dcb5ac5ca874c43c7319768f9788be6393
Kevin Atkins <kevindewayneatkins@gmail.com>
2018-09-07 09:05:18 -0700

    added navbar w/ links to index.php

--- epic/index.php
+++ epic/index.php
@@ -8,12 +8,19 @@
 	</head>
 	<body>
 		<header>
+			<!-- Navbar -->
 			<nav class="topnav">
-
+				<a href="index.php">Home</a>
+				<a href="#">Contact Sheet - Wireframe</a>
+				<a href="#">Conceptual Model</a>
+				<a href="#">Use Cases</a>
+				<a href="#">Dante</a>
+				<a href="#">Serena</a>
 			</nav>
 		</header>
 		<main>
-
+			<h1>Executive Summary</h1>
+			<p></p>
 		</main>
 	</body>
 	<footer>
