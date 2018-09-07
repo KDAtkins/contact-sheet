4aa94b0f0118bf24c0c4d76751a15250bb8f23b3
Kevin Atkins <kevindewayneatkins@gmail.com>
2018-09-07 12:08:58 -0700

    updated navbar links

--- epic/index.php
+++ epic/index.php
@@ -11,11 +11,11 @@
 			<!-- Navbar -->
 			<nav class="topnav">
 				<a href="index.php">Home</a>
-				<a href="#">Contact Sheet - Wireframe</a>
-				<a href="#">Conceptual Model</a>
-				<a href="#">Use Cases</a>
-				<a href="#">Dante</a>
-				<a href="#">Serena</a>
+				<a href="contact-sheet-wireframe.php">Contact Sheet - Wireframe</a>
+				<a href="conceptual-model.php">Conceptual Model</a>
+				<a href="use-cases.php">Use Cases</a>
+				<a href="serena.php">Serena</a>
+				<a href="dante.php">Dante</a>
 			</nav>
 		</header>
 		<main>
