64e8da724449cf560a8007684f855d6c231161d1
Kevin Atkins <kevindewayneatkins@gmail.com>
2018-09-07 12:07:36 -0700

    completed Serena's interaction flow

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -11,15 +11,32 @@
 			<!-- Navbar -->
 			<nav class="topnav">
 				<a href="index.php">Home</a>
-				<a href="#">Contact Sheet - Wireframe</a>
-				<a href="#">Conceptual Model</a>
-				<a href="#">Use Cases</a>
-				<a href="#">Dante</a>
-				<a href="#">Serena</a>
+				<a href="contact-sheet-wireframe.php">Contact Sheet - Wireframe</a>
+				<a href="conceptual-model.php">Conceptual Model</a>
+				<a href="use-cases.php">Use Cases</a>
+				<a href="serena.php">Serena</a>
+				<a href="dante.php">Dante</a>
 			</nav>
 		</header>
 		<main>
-
+			<h1>Use Cases</h1>
+			<h2>Serena's Interaction Flow</h2>
+			<div class="flowlist">
+				<ol>
+					<li>Serena accesses site</li>
+					<li>She logs into account</li>
+					<li>Site directs her to her profile dashboard which presents outreach forms assigned to her profile.</li>
+					<li>She selects one of the assigned forms that presents an option to view contacts previously obtained or to initiate outreach mode.</li>
+					<li>If she selects the option to view contacts obtained, application will then navigate and display contacts with the abilty to filter by date, city, state, & gender.</li>
+					<li>If she selects the option to initiate "outreach" mode, the application will open a form for individuals to input requested personal information</li>
+				</ol>
+			</div>
+			<h2>Dante's Interaction Flow</h2>
+			<div class="flowlist">
+				<ol>
+					<li></li>
+				</ol>
+			</div>
 		</main>
 	</body>
 	<footer>
